8c0a554ab3c693e1487ed0b8480a1c33f7f31bbc
PitPed <srpitped@gmail.com>
2024-04-28 19:30:00 +0200

    Handlig loging

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import SignupForm from './src/components/SignupForm/SignupForm';
 import { islogged, logSess, login, setSessionId } from './src/HTTP Requests/auth';
 import Header from './src/components/Header/Header';
 import { AxiosError, AxiosResponse } from 'axios';
+import TextPost from './src/components/TextPost/TextPost';
+import Navigation from './src/components/Navigation/Navigation';
 //import RecipeForm from './src/components/RecipeForm/RecipeForm';
 
 export default function App() {
@@ -12,14 +14,14 @@ export default function App() {
   const [isSigning, setIsSigning] = useState(false);
 
   function loginHandler(username: string, password: string): void {
-    function handleValidLogin(response: AxiosResponse){
+    function handleValidLogin(response: AxiosResponse) {
       console.log(response.data.message);
       setSessionId(response.data.session)
       logSess();
       setIsLoggedIn(true);
-      islogged().then(response=>console.log(response))
+      islogged().then(response => console.log(response))
     }
-    function handleIvalidLogin(error: AxiosError){
+    function handleIvalidLogin(error: AxiosError) {
       console.log(error.message);
       setIsLoggedIn(false);
     }
@@ -32,7 +34,7 @@ export default function App() {
     setIsSigning((previousValue) => { return !previousValue });
   }
 
-  function logoutHandler () {
+  function logoutHandler() {
     setIsLoggedIn(false);
   }
 
@@ -57,4 +59,3 @@ const styles = StyleSheet.create({
     height: '100%',
   }
 });
-
