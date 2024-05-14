049ed74f6fb896107741e7c8477b93a6be6dba45
Pedro <spain@inwebit.com>
2024-05-14 11:58:55 +0200

    Changed Type Names

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,16 @@
-export type Image = {
-    source: string,
-    alt: string
-}
-
-export type User = {
-    username: string,
-    profilePic: Image,
-}
+export type ImageType = {
+  imageId: string;
+  source: string;
+  alt: string;
+};
+export type UserType = {
+  username: string;
+  profilePic: ImageType;
+};
 export type PostType = {
-    title: string,
-    body: string,
-    votes: number,
-    user: User
-    images: Image[]
-}
+  title: string;
+  body: string;
+  votes: number;
+  user: UserType;
+  images: ImageType[];
+};
